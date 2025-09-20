c5f7efee34fad330b1f4c39d0be7fa6ab2b0ba82
Joao Meyer <1994meyer@gmail.com>
2025-09-20 08:32:40 -0300

    Started navbar

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,35 @@
 <template>
   <UApp class="h-full">
+    <template #left>
+      <NuxtLink
+        to="/"
+        class="flex items-center gap-2"
+      >
+        <UIcon
+          name="i-heroicons-squares-2x2"
+          class="w-6 h-6"
+        />
+        <span class="font-bold text-lg">MultiChess</span>
+      </NuxtLink>
+    </template>
+
+    <UButton
+      to="/"
+      variant="ghost"
+      size="sm"
+    >
+      Home
+    </UButton>
+    <UButton
+      size="sm"
+      @click="async () => {
+        const { $trpc } = useNuxtApp()
+        const game = await $trpc.game.create.mutate()
+        await navigateTo(`/g/${game.id}`)
+      }"
+    >
+      New Game
+    </UButton>
     <NuxtPage />
   </UApp>
 </template>
