9890fda831b49ee2033a3e7bf9c6ccd2b71b2ef0
meyer1994 <1994meyer@gmail.com>
2026-07-07 21:43:09 -0300

    Apply remaining workspace updates

--- app/plugins/trpc.ts
+++ app/plugins/trpc.ts
@@ -1,18 +1,17 @@
-import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
-import { loggerLink } from '@trpc/client'
+import { createTRPCNuxtClient } from 'trpc-nuxt/client'
+import { httpLink, loggerLink, type TRPCLink } from '@trpc/client'
 import type { AppRouter } from '~~/server/trpc/index'
 
 export default defineNuxtPlugin(() => {
+  const links: TRPCLink<AppRouter>[] = []
+  if (import.meta.client) links.push(loggerLink())
+  links.push(httpLink({ url: '/api/trpc' }))
+
   /**
    * createTRPCNuxtClient adds a `useQuery` composable
    * built on top of Nuxt's `useLazyAsyncData`.
    */
-  const trpc = createTRPCNuxtClient<AppRouter>({
-    links: [
-      loggerLink(),
-      httpBatchLink({ url: '/api/trpc' }),
-    ],
-  })
+  const trpc = createTRPCNuxtClient<AppRouter>({ links })
 
   return { provide: { trpc } }
 })
